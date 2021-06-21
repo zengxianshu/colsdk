e177e670a458feaedc04b7943880b3d4ec273c8a
zengxsh <zengxsh@col.com>
2021-06-21 17:15:41 +0800

    Revert "1.2.0"
    This reverts commit 8b98b355c5ee71f345eff15bc8052ce0d4c91a88.

---
+++ iOS端-17kSDK集成/LocalPod/Reader17k/Reader17k/Reader17kSDK.xcframework/ios-arm64_i386_x86_64-simulator/Reader17kSDK.framework/Headers/Reader17kSDK.h
@@ -0,0 +1,23 @@
+//
+//  Reader17kSDK.h
+//  Reader17kSDK
+//
+//  Created by Ink on 2021/1/4.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Reader17kSDK.
+FOUNDATION_EXPORT double Reader17kSDKVersionNumber;
+
+//! Project version string for Reader17kSDK.
+FOUNDATION_EXPORT const unsigned char Reader17kSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Reader17kSDK/PublicHeader.h>
+
+#import <Reader17kSDK/XLPageSegmentedTitleView.h>
+#import <Reader17kSDK/XLPageViewController.h>
+#import <Reader17kSDK/XLPageViewControllerConfig.h>
+#import <Reader17kSDK/XLPageViewControllerUtil.h>
+#import <Reader17kSDK/XLPageBasicTitleView.h>
+#import <Reader17kSDK/XLPageTitleCell.h>
